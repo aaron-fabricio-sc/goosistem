5addee8514eb3616b84cac19bab06a029a9996db
aaron fabricio <aaronfabricio00@gmail.com>
2021-08-19 16:43:21 -0400

    se cambio el numero del formulario

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ const enviarFormulario = () => {
   let negocio = document.getElementById("negocio").value;
 
   let mensaje = document.getElementById("mensaje").value;
-  let numero = +59170668135;
+  let numero = +59169883567;
   var win = window.open(
     `https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${nombre}
 %20${apellido},tengo%20un%20negocio%20de:${negocio},%20Asunto:%20${mensaje}`,
